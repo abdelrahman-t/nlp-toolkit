7a1c8999c40039437147e0926252d16f563b49a3
Abdelrahman <abdurrahman.talaat@gmail.com>
2018-12-16 01:15:14 +0300

    Expose Farasa NER functionality in summarization module

--- nlp_toolkit/tools/summarization.py
+++ nlp_toolkit/tools/summarization.py
@@ -1,8 +1,9 @@
 """Summarization."""
 import logging
-from typing import List
+from typing import List, Tuple
 
-from gensim.summarization import keywords
+from fuzzywuzzy import process
+from gensim.summarization import keywords as _extract_keywords
 from gensim.summarization.summarizer import summarize as _summarize
 
 from .farasa import Farasa
@@ -14,7 +15,12 @@ farasa = Farasa()
 
 
 def summarize(text, ratio) -> str:
-    """Summarize a piece of text."""
+    """
+    Summarize a piece of text.
+
+    :param text: text to summarize.
+    :param ratio: summarization ratio, 1.0 to return the original text without summarization.
+    """
     try:
         summary = _summarize(
             _preprocess_arabic_text(text, remove_emails_urls_html=True),
@@ -27,7 +33,22 @@ def summarize(text, ratio) -> str:
     return summary or text
 
 
-def extract_keywords(text: str, pos_filter: List[str]) -> List[str]:
+def extract_keywords(text: str, pos_filter: List[str], top_n: int = None) -> List[str]:
     """Extract list of keywords from text."""
     text = _preprocess_arabic_text(text, remove_emails_urls_html=True)
-    return keywords(farasa.filter_pos(text, keep=pos_filter), split=True)
+
+    return _extract_keywords(
+        farasa.filter_pos(text, keep=pos_filter),
+        split=True
+    )[:top_n]
+
+
+def extract_entities(text: str) -> List[Tuple[str, str]]:
+    """
+    Extract named entities in text.
+
+    :param text: text to extract entities from.
+    :returns: list of (entity, entity_type) pairs, e.g. [('egypt', 'LOC')]
+    """
+    text = _preprocess_arabic_text(text, remove_emails_urls_html=True)
+    return farasa.get_named_entities(text)
