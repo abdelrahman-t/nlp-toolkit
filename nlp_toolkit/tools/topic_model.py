2617c5c91d369279fe73ea213709a4c29ffc6605
Abdelrahman Talaat <abdurrahman.talaat@gmail.com>
2019-10-14 07:28:30 +0300

    Refactor topic modeling

--- nlp_toolkit/tools/topic_model.py
+++ nlp_toolkit/tools/topic_model.py
@@ -1,12 +1,12 @@
 """Topic modeling."""
 import logging
-from collections import Counter
+from functools import lru_cache, partial
 from multiprocessing import Pool
 from operator import itemgetter as get
-from typing import (Any, Dict, Generator, Iterable, List, Optional, Sequence,
-                    Set, Tuple, Union)
+from typing import (Dict, Generator, Iterable, List, Optional, Sequence, Set,
+                    Tuple, Union)
 
-import nltk
+import dill
 from functional import seq
 from gensim.corpora.dictionary import Dictionary
 from gensim.models import LdaMulticore, Phrases
@@ -15,12 +15,40 @@ from nltk import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from tqdm import tqdm
 
+import nlp_toolkit.dependencies as dependencies
+
 from .farasa import Farasa
+from .pos import filter_pos
+from .lemmatization import lemmatize
 from .utils import _preprocess_arabic_text, setup_logger
 
 LOGGER = setup_logger('topic-models', logging.DEBUG)
 
-nltk.download('punkt')
+
+@lru_cache(maxsize=2)
+def load_topic_model(model_id: int) -> 'TopicModel':
+    """
+    Load topic model.
+
+    :param model_id: model id.
+    :raises: ValueError if no model with specified model_id can not be found.
+    """
+    model_config = dependencies.get_topic_model_config(model_id)
+    model_config['topic_names'] = {
+        int(key): value for key, value in model_config['topic_names'].items()
+    }
+
+    model = TopicModel.load(model_config['path'])
+    model.predict = partial(model.predict,  # type: ignore
+                            topics_map=model_config['topic_names'],
+                            num_topics=model_config['predict_top'])
+
+    return model
+
+
+def infer_topic(model_id: int, document: str):
+    """Infer topic."""
+    return load_topic_model(model_id).predict(document)  # type: ignore
 
 
 class TopicModel:
@@ -30,8 +58,8 @@ class TopicModel:
     Filtering parts of speech is currently done using tools.Farasa.
     """
 
-    def __init__(self, pos_to_use: Sequence[str], stop_words: Set[str], min_df: Union[int, float] = 5,
-                 max_df: Union[int, float] = 0.85, num_workers: int = 1):
+    def __init__(self, pos_to_use: List[str], stop_words: Union[Set[str], List[str], str],
+                 min_df: Union[int, float] = 5, max_df: Union[int, float] = 0.85, num_workers: int = 1):
         """
         Initialize model.
 
@@ -40,6 +68,8 @@ class TopicModel:
          'V', 'NOUN', 'PRON', 'ADJ', 'NUM',
          'CONJ', 'PART', 'NSUFF', 'CASE', 'FOREIGN',
          'DET', 'PREP', 'ABBREV', 'PUNC']
+
+        :param stop_words: list/set of stop words or filepath to the file containing the stop words.
 
         :param max_df: When building the vocabulary ignore terms that have a document
         frequency strictly higher than the given threshold (corpus-specific
@@ -51,6 +81,12 @@ class TopicModel:
 
         :param num_workers: Number of worker to use for preprocessing and training.
         """
+        if isinstance(stop_words, str):
+            stop_words = open(stop_words).read().split('\n')
+
+        if isinstance(stop_words, list):
+            stop_words = set(stop_words)
+
         self.pos_to_use = pos_to_use
         self.num_workers = num_workers
 
@@ -65,6 +101,8 @@ class TopicModel:
         self.trigram_model: Optional[Phraser] = None
         self.id2word: Optional[Dict] = None
 
+        self._farasa: Optional[Farasa] = None
+
     @staticmethod
     def _init_pool():
         """
@@ -73,7 +111,7 @@ class TopicModel:
         Ran only one.
         """
         global farasa
-        farasa = Farasa()
+        farasa = Farasa(singelton=False)
 
     def preprocess_document(self, document: str) -> str:
         """
@@ -89,12 +127,17 @@ class TopicModel:
                                        remove_hashtags_mentions=True)
 
     def _unit_of_work(self, pos_to_use: List[str], document: str) -> str:
-        """Apply unit of work."""
+        """
+        Apply unit of work.
+
+        :param pos_to_use: Parts of speech to keep.
+        :param document: Document to process.
+        """
         global farasa
 
         return farasa.filter_pos(  # type: ignore
             self.preprocess_document(document),
-            pos_to_use
+            parts_of_speech_to_keep=pos_to_use
         )
 
     def preprocess_documents(self, documents: Sequence[str]) -> Generator[str, None, None]:
@@ -105,10 +148,12 @@ class TopicModel:
         """
         progress = tqdm(total=len(documents))
 
-        results = []
+        LOGGER.info('Launching %d workers..', self.num_workers)
         pool = Pool(self.num_workers, initializer=self._init_pool)
 
-        LOGGER.info('Preprocessing documents..')
+        LOGGER.info('Preprocessing documents using %d workers..', self.num_workers)
+
+        results = []
         for document in documents:
             result = pool.apply_async(
                 self._unit_of_work,
@@ -231,6 +276,7 @@ class TopicModel:
         """
         tokens = (seq([document])
                   .map(self.preprocess_document)
+                  .map(lemmatize)  # type: ignore
                   .map(self.tokenize)
                   .map(self.create_trigrams)
                   .flat_map(self.id2word.doc2bow)  # type: ignore
@@ -241,6 +287,8 @@ class TopicModel:
             seq(self._lda_model[tokens][0])
             .sorted(key=lambda x: -x[1])
             .map(get(0))
+            .filter(None)
+            .distinct()
             .take(num_topics)
         )
 
@@ -249,75 +297,22 @@ class TopicModel:
 
         return topics.to_list()
 
-
-class CategoryModel:
-    """
-    Create a category model.
-
-    Documents are represented using their TF-IDF scores.
-    Classifier is trained using LigthGBM.
-    """
-
-    def __init__(self, discard_categories: float, min_df: Union[int, float] = 5, max_df: Union[int, float] = 0.85):
+    @staticmethod
+    def load(path: str) -> 'TopicModel':
         """
-        Initialize model.
+        Load model.
 
-        :param discard_categories: keep only categories that are above this ratio.
-
-        :param max_df: When building the vocabulary ignore terms that have a document
-        frequency strictly higher than the given threshold (corpus-specific
-        stop words).
-
-        :param min_df: When building the vocabulary ignore terms that have a document
-        frequency strictly lower than the given threshold. This value is also
-        called cut-off in the literature.
+        :param path: path to the model.
         """
-        self.discard_categories = discard_categories
+        return dill.load(open(path, 'rb'))
 
-        self.min_df = min_df
-        self.max_df = max_df
-
-        self.vectorizer = TfidfVectorizer(min_df=self.min_df, max_df=self.max_df)
-
-    def preprocess_document(self, document: str) -> str:
+    def save(self, path: str):
         """
-        Preprocess document.
+        Save model.
 
-        :param document: document to preprocess.
+        :param path: path to save the model to.
         """
-        return _preprocess_arabic_text(document,
-                                       remove_non_arabic=True,
-                                       remove_punctuation=True,
-                                       remove_numbers=True,
-                                       remove_emails_urls_html=True,
-                                       remove_hashtags_mentions=True)
+        farasa: Farasa = self.__dict__.pop('_farasa')
+        dill.dump(self, open(path, 'wb'))
 
-    def preprocess_corpus(self, X: Sequence[str], y: Sequence[str]) -> Generator[Tuple[str, Any], None, None]:
-        """
-        Preprocess corpus.
-
-        :param X: training data.
-        :param y: labels.
-        """
-        y_counts = {key: value / len(y) for key, value in Counter(y).items()}
-
-        for document, label in seq(zip(X, y)).filter(lambda item: y_counts[item[1]] > self.discard_categories):
-            yield self.preprocess_document(document), label
-
-    def fit(self, X: Sequence[str], y: Sequence[str]):
-        """
-        Fit model.
-
-        :param X: training data.
-        :param y: labels.
-        """
-        _X, _y = [], []
-        progress = tqdm(total=len(X))
-
-        for document, label in filter(lambda item: item[0] != '', self.preprocess_corpus(X, y)):
-            _X.append(document)
-            _y.append(label)
-
-            progress.update()
-
-        self.vectorizer = self.vectorizer.fit(_X, _y)
+        self._farasa = farasa
